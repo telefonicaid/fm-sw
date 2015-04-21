a508ce7615436ac76d800d97540acee935785d48
Manuel <manuel.casasbarrado@telefonica.com>
2015-04-21 12:42:18 +0200

    fm service

---
+++ js/radiofm_service.js
@@ -0,0 +1,83 @@
+'use strict';
+
+/* This service is used to offer settings to privileged apps. */
+(function (exports) {
+  function RadioFMService() {
+    this.mozFMRadio = navigator.mozFMRadio;
+  }
+
+  RadioFMService.prototype = {
+    _observers: [],
+
+    handleRequest: function ss_handleRequest(msg) {
+      switch (msg.type) {
+        case 'get':
+          this.handleGetMessage(aMsg);
+          break;
+        case 'set':
+          this.handleSetMessage(aMsg);
+          break;
+        case 'listener':
+          this.handleListenerMessage(aMsg);
+          break;
+        default:
+          this.handleMessage(aMsg);
+          break;
+      }
+    },
+
+    handleGetMessage = function sw_handleGetMessage(msg) {
+      if (!msg.name) {
+        console.error('Missing parameter: name');
+        return;
+      }
+
+      if(this.mozFMRadio[msg.name]) {
+        msg.source.postMessage({type: 'get', name: msg.name,
+          value: this.mozFMRadio[msg.name]});
+      }
+    },
+
+    handleSetMessage = function sw_handleSetMessage(msg) {
+      if (!msg.name || typeof msg.value === 'undefined') {
+        console.error('Missing parameter: name');
+        return;
+      }
+
+      if(this.mozFMRadio[msg.name]) {
+        this.mozFMRadio[msg.name] = msg.value;
+        msg.source.postMessage({type: 'set', name: msg.name,
+          value: this.mozFMRadio[msg.name] == msg.value});
+      }
+    },
+
+    handleListenerMessage = function sw_handleListenerMessage(msg) {
+      if (!msg.name) {
+        console.error('Missing parameter: name');
+        return;
+      }
+
+      if(this.mozFMRadio[msg.name]) {
+        this.mozFMRadio[msg.name] = function (value) {
+          msg.source.postMessage({type: 'listener', name: msg.name,
+            value: value});
+        };
+      }
+    },
+
+    handleMessage = function sw_handleMessage(msg) {
+      if(this.mozFMRadio[msg.type]) {
+        msg.source.postMessage({type: 'listener',
+          value: this.mozFMRadio[msg.type](msg.args)});
+      }
+    },
+
+    respondRequest: function ss_respondRequest(response) {
+      navigator.serviceWorker.ready.then(sw => {
+        sw.active && sw.active.postMessage(response);
+      });
+    }
+  };
+
+  exports.RadioFMService = new RadioFMService();
+}(window));
---
+++ sw.js
@@ -0,0 +1,85 @@
+'use strict';
+
+// This is a very basic sample Service Worker (SW) that  acts as a server for
+// navigator.connect. I'm going to mark with a comment where the app MUST
+// add some extra code to use the navigator.connect SHIM
+// So if you just want to know that, search for:
+// ADDED FOR SHIM
+
+
+function debug(str) {
+  console.log('MANU -> ' + str);
+}
+
+// ADDED FOR SHIM: Import the shim script
+this.importScripts("/fm-sw/shim/navigator_connect_shim_sw.js");
+// END ADDED FOR SHIM
+
+debug('SW importScripts executed (hopefully)!');
+
+this.addEventListener('install', function(evt) {
+  debug('SW Install event');
+});
+
+this.addEventListener('activate', function(evt) {
+  debug('SW activate event');
+});
+
+this.addEventListener('fetch', function(evt) {
+  debug('SW fetch event');
+});
+
+this.mozFMRadio = navigator.mozFMRadio;
+
+this.onconnect = function(msg) {
+  debug("SW onconnect event");
+  for(var i in msg){
+    debug('SW --->' +i+':'+msg[i]);
+  }
+  debug("SW onconnect: We should have a port here on msg.source: " +
+        JSON.stringify(msg.source));
+  // msg.source should have the endpoint to send and receive messages,
+  // so we can do:
+  msg.acceptConnection(true);
+  msg.source.onmessage = function(aMsg) {
+    var msg = aMsg.data;
+    if (!msg.type || !msg.name) {
+      debug('Message received bad formed');
+      return;
+    }
+
+    // We need to delegate this work to the app because SW does not have access
+    // to the device APIs
+    self.clients.matchAll().then(res => {
+      if (!res.length) {
+        debug('SW SETTING Error: no clients are currently controlled.');
+      } else {
+        debug('SW SETTING Sending...');
+        res[0].postMessage(msg);
+      }
+    });
+  };
+
+  this.msgConnectionChannel = msg.source;
+};
+
+this.addEventListener('message', function(evt) {
+  // This is a hack caused by the lack of dedicated MessageChannels... sorry!
+  debug('SW onmessage ---> '+ JSON.stringify(evt.data));
+  // ADDED FOR SHIM
+  // Since we're using the same channel to process messages comming from the main
+  // thread of the app to the SW, and messages coming from the navigator.connect
+  // shim, we have to distinguish them here
+  if (this.NCShim.isInternalMessage(evt)) {
+    debug('SW es msg interno. no ejectuar esto');
+    return;
+  }
+  // END ADDED FOR SHIM
+
+  // Your code here
+  debug("SW We got a message for us!");
+  this.msgConnectionChannel.postMessage(evt.data);
+});
+
+
+
