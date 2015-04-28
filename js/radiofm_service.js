7c8ee0a1b1f02287f87b958e652afad585f936b5
Manuel <manuel.casasbarrado@telefonica.com>
2015-04-28 09:05:14 +0200

    minor issue

--- js/radiofm_service.js
+++ js/radiofm_service.js
@@ -80,6 +80,7 @@ console.info('MANU - MSG RECEIVED' + JSON.stringify(msg.data));
 console.info('Listener added  --> ' + msg.data.name);
 console.info('mozFMRadio has method? --> ' + this.mozFMRadio[msg.data.name] ? 'si' : 'no');
       if(this.mozFMRadio[msg.data.name]) {
+        console.info('Dentro del if!');
         this.mozFMRadio[msg.data.name] = function (value) {
           console.info('Listener triggered  --> ' + msg.data.name);
           msg.channel.postMessage({type: 'listener', name: msg.data.name,
