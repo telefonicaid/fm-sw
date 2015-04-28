e2ce5ffb9ec9fbe1f4188c654ca72f1d6d144d63
Manuel <manuel.casasbarrado@telefonica.com>
2015-04-28 09:23:22 +0200

    minor issue

--- js/radiofm_service.js
+++ js/radiofm_service.js
@@ -80,9 +80,9 @@ console.info('MANU - MSG RECEIVED' + JSON.stringify(msg.data));
 console.info('Listener added  --> ' + msg.data.name);
       this.mozFMRadio[msg.data.name] = function (evt) {
         console.info('Listener triggered  --> ' + msg.data.name);
-        console.info('Listener triggered  --> ' + JSON.stringify(evt));
+        console.info('Listener triggered  --> ' + evt);
         msg.channel.postMessage({type: 'listener', name: msg.data.name,
-          value: value});
+          value: true});
       };
     },
 
